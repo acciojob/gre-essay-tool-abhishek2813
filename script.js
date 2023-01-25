040eeb80f9c67cf79a7c4b060001476ba61a7181
Abhishek Pandey <98042094+abhishek2813@users.noreply.github.com>
2023-01-25 14:02:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your code here
+function countfun(){
+ let wordcount =document.getElementById("evaluatedText").value;
+    let count = wordcount.length;
+    let countword = document.getElementById("wordCount").innerHTML=count;
+}
